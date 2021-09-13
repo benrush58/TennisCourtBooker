0dd448481e7668651fa4f7cb48a79447ed710269
Benjamin Rush <rush.b@husky.neu.edu>
2021-09-13 17:00:49 -0400

    Separated some code into its own functions, deleted extra print statements, got rid of default values, and overall cleaned the code up

--- FieldClubTennis.py
+++ FieldClubTennis.py
@@ -8,79 +8,6 @@ import datetime
 s = requests.Session()
 
 
-
-
-# start = input("What date do you want to play? Type in the form of YYYY-MM-DD X:XX A/PM. An example is 2021-08-31 6:00 PM\n")
-# print(start)
-# time1 = start.split(" ")
-# print(time1)
-# timeFinal = time1[-2] + " " + time1[-1]
-currDate = "2021-09-06"
-
-# print(timeFinal)
-# datePicker = input("Enter date of play in the form of Tuesday, Aug 31st, 2021\n")
-# print(datePicker)
-# duration = input("How many minutes you want to book the court for. Just type the digits. For example, 60\n")
-# print(duration)
-# courtNumber = input("What court do you want to book? For example, 5 or 12\n")
-# print(courtNumber)
-# type = input("Singles or Doubles? Capitalize the word\n")
-# print(type)
-# number = input("How many people? For example, 2 or 4\n")
-# print(number)
-yesOrNo = input("Would you like the code to run now or at a different time? yes for now, and no for later\n")
-if yesOrNo == "no":
-    timeRun = input("What time would you like the code to run? For example, 0:00 is midnight and 17:00 is 5pm\n")
-    timeSplit = timeRun.split(":")
-    timeRunFinal = datetime.time(int(timeSplit[0]), int(timeSplit[1]), 0, 0)
-    print(timeRunFinal)
-    now = datetime.datetime.now()
-    while now.minute != timeRunFinal.minute or now.hour != timeRunFinal.hour:
-        now = datetime.datetime.now()
-        print("Waiting to run code at " + str(timeRunFinal))
-        time.sleep(10)
-
-
-
-
-
-start = "2021-08-30 3:00 PM"
-datePicker = "Monday, Aug 30th, 2021"
-timeFinal = "3:00 PM"
-duration = "60"
-courtNumber = "5"
-number = "4"
-type = "Doubles"
-
-f = open("credentials.txt", "r")
-username = f.readline()
-password = f.readline()
-print(username + password)
-f.close()
-
-
-
-
-
-# Getting the CSRF token
-tokenUrl = "https://login.reservemycourt.com/login"
-response = s.get(tokenUrl)
-soup = BeautifulSoup(response.text, "html.parser")
-# print(soup.prettify())
-
-token = soup.find("meta", attrs={"name":"csrf-token"})
-# print(token)
-tokenFinal = str(token)[15:55]
-# print(tokenFinal)
-
-# Getting the cookies
-cookies = response.headers["Set-Cookie"]
-cookiesList = cookies.replace(",", ";")
-cookiesSplit = cookiesList.split(";")
-# print(cookiesSplit)
-cookieTotal = cookiesSplit[0] + ";" + cookiesSplit[6] + ";" + cookiesSplit[13]
-# print(cookieTotal)
-
 courtToID = {
     "1": "AMN-0ee28cd9468647b7b1accba1fead3e72",
     "2": "AMN-b32bb58931074c6890c6b8c244183c36",
@@ -116,118 +43,168 @@ IDToCourt = {
 }
 
 
-headersLogin = {
-    "authority": "login.reservemycourt.com",
-    "method": "POST",
-    "path": "/login",
-    "scheme": "https",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q=0.9",
-    "cache-control": "max-age=0",
-    "content-length": "97",
-    "content-type": "application/x-www-form-urlencoded",
-    "cookie": cookieTotal,
-    "origin": "https://login.reservemycourt.com",
-    "referer": "https://login.reservemycourt.com/login",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"
-}
+def login():
+    # Get the username and password
+    f = open("credentials.txt", "r")
+    username = f.readline()
+    password = f.readline()
+    f.close()
 
-dataLogin = {
-    "_token": tokenFinal,
-    "email": username,
-    "password": password
-}
+    #Get cookies and CSRF Token
+    tokenUrl = "https://login.reservemycourt.com/login"
+    response = s.get(tokenUrl)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-websiteLogin = "https://login.reservemycourt.com/login"
+    token = soup.find("meta", attrs={"name": "csrf-token"})
+    tokenFinal = str(token)[15:55]
 
-response = s.post(websiteLogin, headers=headersLogin, data=dataLogin)
+    cookies = response.headers["Set-Cookie"]
+    cookiesList = cookies.replace(",", ";")
+    cookiesSplit = cookiesList.split(";")
+    cookieTotal = cookiesSplit[0] + ";" + cookiesSplit[6] + ";" + cookiesSplit[13]
+    headersLogin = {
+        "authority": "login.reservemycourt.com",
+        "method": "POST",
+        "path": "/login",
+        "scheme": "https",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "content-length": "97",
+        "content-type": "application/x-www-form-urlencoded",
+        "cookie": cookieTotal,
+        "origin": "https://login.reservemycourt.com",
+        "referer": "https://login.reservemycourt.com/login",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"
+    }
 
+    dataLogin = {
+        "_token": tokenFinal,
+        "email": username,
+        "password": password
+    }
 
-calUrl = "https://app.reservemycourt.com/clubs/CLB-299e4786f534461ea8b2dce22bbe1b46/fullCalendarReservations/" + currDate
-cal = s.get(calUrl)
-print(cal.text)
-for x in cal.json():
-    start = x.get("start").split(" ")
-    end = x.get("end").split(" ")
-    print("Court " + IDToCourt[x.get("resourceId")] + ": " + start[1] + "-" + end[1])
+    websiteLogin = "https://login.reservemycourt.com/login"
 
-
-
-cook = s.get("https://app.reservemycourt.com/reservations")
-cookies = cook.headers["Set-Cookie"]
-cookiesList = cookies.replace(",", ";")
-cookiesSplit = cookiesList.split(";")
-cookieTotal2 = cookiesSplit[0] + ";" + cookiesSplit[6] + ";" + cookiesSplit[13]
-
-
-# Getting the CSRF token
-tokenUrl = "https://app.reservemycourt.com/reservations"
-response = s.get(tokenUrl)
-soup = BeautifulSoup(response.text, "html.parser")
-# print(soup.prettify())
-
-token = soup.find("meta", attrs={"name":"csrf-token"})
-# print(token)
-tokenFinal = str(token)[15:55]
-# print(tokenFinal)
-
-headersBook = {
-    "authority": "app.reservemycourt.com",
-    "method": "POST",
-    "path": "/reservations",
-    "scheme": "https",
-    "accept": "*/*",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q=0.9",
-    "content-length": "629",
-    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "cookie": cookieTotal2,
-    "origin": "https://app.reservemycourt.com",
-    "referer": "https://app.reservemycourt.com/clubs/CLB-299e4786f534461ea8b2dce22bbe1b46/reservations",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
-    "x-requested-with": "XMLHttpRequest"
-}
+    response = s.post(websiteLogin, headers=headersLogin, data=dataLogin)
+    return response
 
 
-dataBook = {
-    "_token": tokenFinal,
-    "start": start,
-    "datePicker": datePicker,
-    "startPicker": timeFinal,
-    "duration": duration,
-    "interval": "Day",
-    "repeat": "1",
-    "amenityIdCheck": courtToID["1"],
-    "amenities[]": courtToID[courtNumber],
-    "private": "1",
-    "authUser": "USR-2319a6d3c81d4827be0cae5cdca33852",
-    "use_type": type,
-    "owner_user_id": "USR-2319a6d3c81d4827be0cae5cdca33852",
-    "attendee_id": "USR-2319a6d3c81d4827be0cae5cdca33852",
-    "event_type_id": "",
-    "participant_count": number,
-    "notes": "",
-    "pro_id": "USR-2319a6d3c81d4827be0cae5cdca33852",
-    "description": "",
-    "min_participants": "1",
-    "max_participants": "2",
-    "clinic_fee_id": "",
-    "private": "0",
-    "join_all_required": "0",
-}
+# Function gets the calender of the day requested
+def calender(date):
+    response = login()
+    calUrl = "https://app.reservemycourt.com/clubs/CLB-299e4786f534461ea8b2dce22bbe1b46/fullCalendarReservations/" + date
+    cal = s.get(calUrl)
+    for x in cal.json():
+        start = x.get("start").split(" ")
+        end = x.get("end").split(" ")
+        print("Court " + IDToCourt[x.get("resourceId")] + ": " + start[1] + "-" + end[1])
 
-websiteBook = "https://app.reservemycourt.com/reservations"
 
-# response = s.post(websiteBook, headers=headersBook, data=dataBook)
+def wait():
+    timeRun = input("What time would you like the code to run? For example, 0:00 is midnight and 17:00 is 5pm\n")
+    timeSplit = timeRun.split(":")
+    timeRunFinal = datetime.time(int(timeSplit[0]), int(timeSplit[1]), 0, 0)
+    print(timeRunFinal)
+    now = datetime.datetime.now()
+    while now.minute != timeRunFinal.minute or now.hour != timeRunFinal.hour:
+        now = datetime.datetime.now()
+        print("Waiting to run code at " + str(timeRunFinal))
+        time.sleep(10)
 
-# print(response.text)
+
+
+def main():
+    date = input("What date do you want to play? Type in the form of YYYY-MM-DD\n")
+    datePicker = input("Enter date of play in the form of Tuesday, Aug 31st, 2021\n")
+    reservations = input("Would you like to see what court times have already been booked on that day? yes or no\n")
+    if reservations == "yes":
+        calender(date)
+    timeFinal = input("What time would you like to play? Enter in the form X:XX A/PM. For example, 7:30 PM\n")
+    start = date + " " + timeFinal
+    duration = input("How many minutes you want to book the court for. Just type the digits. For example, 60\n")
+    courtNumber = input("What court do you want to book? For example, 5 or 12\n")
+    type = input("Singles or Doubles? Capitalize the word\n")
+    number = input("How many people? For example, 2 or 4\n")
+    yesOrNo = input("Would you like the code to run now or at a different time? yes for now, and no for later\n")
+    if yesOrNo == "no":
+        wait()
+    login()
+
+    cook = s.get("https://app.reservemycourt.com/reservations")
+    cookies = cook.headers["Set-Cookie"]
+    cookiesList = cookies.replace(",", ";")
+    cookiesSplit = cookiesList.split(";")
+    cookieTotal2 = cookiesSplit[0] + ";" + cookiesSplit[6] + ";" + cookiesSplit[13]
+
+
+    # Getting the CSRF token
+    tokenUrl = "https://app.reservemycourt.com/reservations"
+    response = s.get(tokenUrl)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    token = soup.find("meta", attrs={"name":"csrf-token"})
+    tokenFinal = str(token)[15:55]
+
+    headersBook = {
+        "authority": "app.reservemycourt.com",
+        "method": "POST",
+        "path": "/reservations",
+        "scheme": "https",
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "content-length": "629",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "cookie": cookieTotal2,
+        "origin": "https://app.reservemycourt.com",
+        "referer": "https://app.reservemycourt.com/clubs/CLB-299e4786f534461ea8b2dce22bbe1b46/reservations",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
+        "x-requested-with": "XMLHttpRequest"
+    }
+
+
+    dataBook = {
+        "_token": tokenFinal,
+        "start": start,
+        "datePicker": datePicker,
+        "startPicker": timeFinal,
+        "duration": duration,
+        "interval": "Day",
+        "repeat": "1",
+        "amenityIdCheck": courtToID["1"],
+        "amenities[]": courtToID[courtNumber],
+        "private": "1",
+        "authUser": "USR-2319a6d3c81d4827be0cae5cdca33852",
+        "use_type": type,
+        "owner_user_id": "USR-2319a6d3c81d4827be0cae5cdca33852",
+        "attendee_id": "USR-2319a6d3c81d4827be0cae5cdca33852",
+        "event_type_id": "",
+        "participant_count": number,
+        "notes": "",
+        "pro_id": "USR-2319a6d3c81d4827be0cae5cdca33852",
+        "description": "",
+        "min_participants": "1",
+        "max_participants": "2",
+        "clinic_fee_id": "",
+        "private": "0",
+        "join_all_required": "0",
+    }
+
+    websiteBook = "https://app.reservemycourt.com/reservations"
+
+    response = s.post(websiteBook, headers=headersBook, data=dataBook)
+
+    print(response.text)
+
+main()
 
